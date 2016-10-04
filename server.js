5b5d14ebd48523109ce2f4c93d8cefe384097a19
Matt Cowan <mattcowan@outlook.com>
2016-10-04 00:27:28 -0400

    Add request and error logging

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const compression = require('compression');
 const express = require('express');
+const expressWinston = require('express-winston');
+const winston = require('winston'); // for transports.Console
 const app = express();
 
 // compress our client side content before sending it over the wire
 app.use(compression());
 
+// request logging
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// error logging
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
 
 // listen for requests
 const listener = app.listen(process.env.PORT, function () {
