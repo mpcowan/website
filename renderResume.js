c108eba53ccf90f03a8a203134c61ffdd79639bb
Matt Cowan <mcowan@atlassian.com>
2019-05-05 15:36:41 -0400

    Moving to netlify

---
+++ renderResume.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const marked = require("marked");
+const path = require("path");
+
+// render resume
+const data = fs.readFileSync(path.join(__dirname, "src/resume.md"), "utf8");
+
+marked(data, (error, content) => {
+  if (error) {
+    console.error(error.message);
+    throw error;
+  }
+
+  const html = fs.readFileSync(
+    path.join(__dirname, "src/resume-wrapper.html"),
+    "utf8"
+  );
+
+  fs.writeFileSync(
+    path.join(__dirname, "src/resume.html"),
+    html.replace("{{RESUME_CONTENT}}", content)
+  );
+});
